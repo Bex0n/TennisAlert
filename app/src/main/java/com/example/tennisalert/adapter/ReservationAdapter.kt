3489981badc6b168e64cba3e63ab3e470a943d68
Jakub Dawidowicz <jd439952@students.mimuw.edu.pl>
2025-03-28 21:13:41 +0100

    Kretv2: Reservation time in UI

--- app/src/main/java/com/example/tennisalert/adapter/ReservationAdapter.kt
+++ app/src/main/java/com/example/tennisalert/adapter/ReservationAdapter.kt
@@ -8,6 +8,8 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.tennisalert.R
 import com.example.tennisalert.model.Reservation
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 class ReservationAdapter(
     private val reservations: List<Reservation>,
@@ -15,8 +17,9 @@ class ReservationAdapter(
 ) : RecyclerView.Adapter<ReservationAdapter.ViewHolder>() {
 
     inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val dateText: TextView = view.findViewById(R.id.text_date)
-        val courtsText: TextView = view.findViewById(R.id.text_courts)
+        val dateText: TextView = view.findViewById(R.id.detail_date)
+        val timeText: TextView = view.findViewById(R.id.detail_time)
+        val courtsText: TextView = view.findViewById(R.id.detail_courts)
         val removeButton: Button = view.findViewById(R.id.button_remove)
     }
 
@@ -28,7 +31,17 @@ class ReservationAdapter(
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val reservation = reservations[position]
-        holder.dateText.text = reservation.date
+        println(reservation.date)
+        var formattedDate = ""
+        try {
+            val polishDate = LocalDate.parse(reservation.date, DateTimeFormatter.ofPattern("dd-M-yyy"))
+            formattedDate = polishDate.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"))
+        }
+        catch (e: Exception) {
+            formattedDate = "Date unknown"
+        }
+        holder.dateText.text = "Date: $formattedDate"
+        holder.timeText.text = "Time: ${reservation.startTime} - ${reservation.endTime}"
         holder.courtsText.text = "Courts: ${reservation.courts.joinToString(", ")}"
         holder.removeButton.setOnClickListener { onRemoveClicked(position) }
     }
